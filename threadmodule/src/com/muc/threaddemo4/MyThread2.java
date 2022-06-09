86c0f775a1a4a749ffc9ce9e4b6a6769e8d5a122
adoifK <17628486615@163.com>
2022-06-10 00:33:28 +0800

    守护线程

---
+++ threadmodule/src/com/muc/threaddemo4/MyThread2.java
@@ -0,0 +1,18 @@
+package com.muc.threaddemo4;
+
+public class MyThread2 extends Thread {
+    @Override
+    public void  run(){
+        //这部分代码就是线程在开启之后执行的代码
+       /* for(int i=0;i<100;i++){
+            System.out.println("线程开启了"+i);
+        }*/
+
+        for(int i=0;i<100;i++)
+        {
+            System.out.println(getName()+"@@@"+i);
+        }
+
+    }
+
+}
