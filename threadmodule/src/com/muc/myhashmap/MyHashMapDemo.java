b1e5456de3401b452edc0f7f82e9c1e967b5f817
adoifK <17628486615@163.com>
2022-06-10 00:29:55 +0800

    hashmap 和hashTable 线程安全性对比

---
+++ threadmodule/src/com/muc/myhashmap/MyHashMapDemo.java
@@ -0,0 +1,72 @@
+package com.muc.myhashmap;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+
+public class MyHashMapDemo {
+    public static void main(String[] args) throws InterruptedException {
+//        method1();//hashmap 线程不安全
+
+//        method2();//hashtable 线程安全
+    }
+
+    private static void method2() throws InterruptedException {
+        Hashtable<String, String> hm = new Hashtable<>();
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 25; i++) {
+                hm.put(i + "", i + "");
+            }
+        });
+
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 25; i < 51; i++) {
+                hm.put(i + "", i + "");
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        System.out.println("----------------------------");
+        //为了t1和t2能把数据全部添加完毕
+        Thread.sleep(1000);
+
+        //0-0 1-1 ..... 50- 50
+
+        for (int i = 0; i < 51; i++) {
+            System.out.println(hm.get(i + ""));
+        }//0 1 2 3 .... 50
+    }
+
+    private static void method1() throws InterruptedException {
+        HashMap<String, String> hm = new HashMap<>();
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 25; i++) {
+                hm.put(i + "", i + "");// i + "" 字符串
+            }
+        });
+
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 25; i < 51; i++) {
+                hm.put(i + "", i + "");
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        System.out.println("----------------------------");
+        //为了t1和t2能把数据全部添加完毕
+        Thread.sleep(1000);
+
+        //0-0 1-1 ..... 50- 50
+
+        for (int i = 0; i < 51; i++) {
+            System.out.println(hm.get(i + ""));
+        }//0 1 2 3 .... 50
+    }
+}
