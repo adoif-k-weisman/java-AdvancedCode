9b23d3b091c1cefabc33050a4804411b7719b1ea
adoifK <17628486615@163.com>
2022-07-03 16:41:26 +0800

    junit单元测试简单学习

---
+++ myjunit/src/com/muc/basicuse/JunitDemo.java
@@ -0,0 +1,19 @@
+package com.muc.basicuse;
+
+import org.junit.Test;
+
+public class JunitDemo {
+    public static void main(String[] args) {
+
+    }
+
+    @Test
+    public void add()
+    {
+        int a=10;
+        int b=20;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+
+}
